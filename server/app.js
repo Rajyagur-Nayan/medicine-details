0d5ab1127144aef0d7ae67662c2cec0717286732
Rajyaguru Nayan <nayanrajyaguru59@gamil.com>
2025-08-22 02:52:07 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:3000", // your frontend URL
-  credentials: true,               // allow cookies
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3001", // your frontend URL
+    credentials: true, // allow cookies
+  })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // routes
-app.get('/', (req, res) => {
-    res.send('API is running 🚀');
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
 });
 
-app.use('/signup', require('./src/routes/user/signup.js'));
-app.use('/login', require('./src/routes/user/login.js'));
-app.use('/delete', require('./src/routes/user/delete.js'))
-app.use('/scan', require('./src/routes/scan/scan.js'))
-app.use("/profile" , require("./src/routes/profile/profile.js"))
+app.use("/signup", require("./src/routes/user/signup.js"));
+app.use("/login", require("./src/routes/user/login.js"));
+app.use("/delete", require("./src/routes/user/delete.js"));
+app.use("/scan", require("./src/routes/scan/scan.js"));
+app.use("/profile", require("./src/routes/profile/profile.js"));
 
-module.exports = app
+module.exports = app;
